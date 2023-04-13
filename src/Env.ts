266cfb4080080b0abf6c5415bab5c90006ba4604
“kieuvu” <kieuvuvuvu@gmail.com>
2023-04-13 16:32:26 +0700

    env entity

---
+++ src/Env.ts
@@ -0,0 +1,5 @@
+import { config } from "dotenv";
+config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+
+export const CREDENTIALS = process.env.CREDENTIALS === "true";
+export const ENV = process.env;
