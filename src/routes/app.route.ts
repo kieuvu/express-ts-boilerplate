7bf3fc0c02d187884c3aa16f90f2088311c2e236
“kieuvu” <kieuvuvuvu@gmail.com>
2023-04-14 10:21:47 +0700

    define route

---
+++ src/routes/app.route.ts
@@ -0,0 +1,11 @@
+import RouteAbstract from "../abstracts/route.abstract";
+import RouteConstant from "../constants/route.constant";
+import IndexAction from "../actions/app/IndexAction";
+
+class AppRoutes extends RouteAbstract {
+  initRoutes() {
+    this._router.get(RouteConstant.APP_ROUTES.INDEX, IndexAction.handle);
+  }
+}
+
+export default new AppRoutes();
