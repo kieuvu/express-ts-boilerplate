ea5b3acd86973a3c6ed0b4ae30a541c12cd8385e
“kieuvu” <kieuvuvuvu@gmail.com>
2023-04-14 15:32:35 +0700

    login route

--- src/routes/api/auth.route.ts
+++ src/routes/api/auth.route.ts
@@ -1,4 +1,5 @@
 import RouteAbstract from "../../abstracts/route.abstract";
+import LoginAction from "../../actions/auth/LoginAction";
 import RegisterAction from "../../actions/auth/RegisterAction";
 import RouteConstant from "../../constants/route.constant";
 import HttpMethod from "../../enums/http.enum";
@@ -6,6 +7,7 @@ import HttpMethod from "../../enums/http.enum";
 class AuthRoutes extends RouteAbstract {
   public override initRoutes(): void {
     this.defineRoute(HttpMethod.POST, RouteConstant.AUTH_ROUTES.REGISTER, RegisterAction);
+    this.defineRoute(HttpMethod.POST, RouteConstant.AUTH_ROUTES.LOGIN, LoginAction);
   }
 }
 
